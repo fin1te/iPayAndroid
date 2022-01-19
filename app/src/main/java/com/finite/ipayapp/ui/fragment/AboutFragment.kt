5d2f79c670146b98784341f49210f1dc9822bdf9
Fin1te <rishabhmehta00@gmail.com>
2022-01-19 05:53:41 +0530

    AboutFragment Added

---
+++ app/src/main/java/com/finite/ipayapp/ui/fragment/AboutFragment.kt
@@ -0,0 +1,19 @@
+package com.finite.ipayapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.finite.ipayapp.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+}
